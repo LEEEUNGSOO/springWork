a1ccfca6bdb41288b28586c06e57394af6c77a62
이응수 <sistsay@gmail.com>
2025-09-08 10:43:07 +0900

    mvc

--- src/main/java/com/multi/demo/controller/HomeController.java
+++ src/main/java/com/multi/demo/controller/HomeController.java
@@ -4,6 +4,8 @@ package com.multi.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class HomeController {
@@ -11,12 +13,14 @@ public class HomeController {
     @GetMapping({"/", "/home"})
     public String home(Model model){
         model.addAttribute("msg", "Spring Legacy (XML) running!");
+
+
         return "home"; // /WEB-INF/views/home.jsp
     }
-    @GetMapping({"/", "/message"})
-    public String message(Model model){
-        model.addAttribute("msg", "Spring Legacy");
-        return "message"; // /WEB-INF/views/message.jsp
+    @GetMapping("/messsage")
+    public String msg(Model model){
+        return "hi";
     }
+
 }
 
