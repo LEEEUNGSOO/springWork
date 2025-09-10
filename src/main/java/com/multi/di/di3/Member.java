b905985c6c5392c06a5ef40de7288319f0a76f5e
이응수 <sistsay@gmail.com>
2025-09-10 13:53:58 +0900

    di

--- src/main/java/com/multi/di/di3/Member.java
+++ src/main/java/com/multi/di/di3/Member.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.annotation.Resource;
+
 
 //보일러 프레이트코드
 @Data
---
+++ src/main/java/com/multi/di/di4/PersonMain.java
@@ -0,0 +1,12 @@
+package com.multi.di.di4;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PersonMain {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/spring/persondi.xml");
+        PersonCollection pCollection  =((PersonCollection)applicationContext.getBean("pCollection"));
+        System.out.println(pCollection);
+    }
+}
