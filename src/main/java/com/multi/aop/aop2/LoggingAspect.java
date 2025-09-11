655131bd23fa80998b814a3892c27b743315c915
이응수 <sistsay@gmail.com>
2025-09-11 17:41:28 +0900

    aop

--- src/main/java/com/multi/aop/aop2/LoggingAspect.java
+++ src/main/java/com/multi/aop/aop2/LoggingAspect.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 @Aspect //what +when +where
 @Component//객체를 생성하라는 annotation
 public class LoggingAspect {
+
     @Before("execution(* com.multi.repository.ActionService.*(..))")
     public void beforeMehod(){
         System.out.println("beforeMehod");//what(공통단 코드)
