ce3fb9ed8f3402a3149cde37112dc98477ca728a
이응수 <sistsay@gmail.com>
2025-09-11 17:35:23 +0900

    aop6

--- src/main/java/com/multi/aop/aop2/LoggingAspect.java
+++ src/main/java/com/multi/aop/aop2/LoggingAspect.java
@@ -1,13 +1,14 @@
 package com.multi.aop.aop2;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect //what +when +where
 @Component//객체를 생성하라는 annotation
 public class LoggingAspect {
-    //@Before("")
+    @Before("execution(* com.multi.repository.ActionService.*(..))")
     public void beforeMehod(){
-        System.out.println("beforeMehod");
+        System.out.println("beforeMehod");//what(공통단 코드)
     }
 }
