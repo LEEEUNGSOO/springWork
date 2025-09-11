e81a04de637064c407d465efdabe290edd87e4a1
이응수 <sistsay@gmail.com>
2025-09-11 15:26:00 +0900

    aop

--- src/main/java/com/multi/aop/aop1/XmlAopMain.java
+++ src/main/java/com/multi/aop/aop1/XmlAopMain.java
@@ -1,5 +1,6 @@
 package com.multi.aop.aop1;
 
+import com.multi.service.MyService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class XmkAopMain {
@@ -7,7 +8,10 @@ public class XmkAopMain {
         ClassPathXmlApplicationContext applicationContext =
                 new ClassPathXmlApplicationContext("/spring/aop1.xml");
         System.out.println(applicationContext
+
         );
+        MyService myService = applicationContext.getBean(MyService.class);
+        System.out.println(myService.performAction("XML"));
 
     }
 
