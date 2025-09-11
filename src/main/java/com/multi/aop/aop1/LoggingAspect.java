2a730bdc97f427b13268434b7a7ca6e5e3addf7c
이응수 <sistsay@gmail.com>
2025-09-11 15:29:58 +0900

    aop3

--- src/main/java/com/multi/aop/aop1/LoggingAspect.java
+++ src/main/java/com/multi/aop/aop1/LoggingAspect.java
@@ -1,4 +1,20 @@
 package com.multi.aop.aop1;
 
-public class LoggingAspect {
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {//LoggingAspect
+    //Aspect은 What +When +Where
+
+    //When Before
+    //* com.multi.service.MyService.*(..))이코든  pointcut을 의미한다 Where
+    @Before("execution(* com.multi.service.MyService.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("logBefore");//What(공통부분)
+    }
+
 }
--- src/main/java/com/multi/aop/aop1/XmlAopMain.java
+++ src/main/java/com/multi/aop/aop1/XmlAopMain.java
@@ -3,13 +3,15 @@ package com.multi.aop.aop1;
 import com.multi.service.MyService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class XmkAopMain {
+public class XmlAopMain {
     public static void main(String[] args) {
+        //applicationContext는 xmlDom을 의미한다..
         ClassPathXmlApplicationContext applicationContext =
                 new ClassPathXmlApplicationContext("/spring/aop1.xml");
         System.out.println(applicationContext
 
         );
+        //MyService는
         MyService myService = applicationContext.getBean(MyService.class);
         System.out.println(myService.performAction("XML"));
 
